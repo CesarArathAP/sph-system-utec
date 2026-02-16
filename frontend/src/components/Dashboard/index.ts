ac715dbaf1a98a32ba441100a2eb4f177cb10a7e
AngelGM876 <parckerpeter037@gmail.com>
2026-02-16 14:56:38 -0600

    chore: centralizar exports del módulo dashboard

---
+++ frontend/src/components/Dashboard/index.ts
@@ -0,0 +1,4 @@
+export { default as DashboardLayout } from './DashboardLayout';
+export { default as Sidebar } from './Sidebar';
+export { default as ScheduleTable } from './ScheduleTable';
+export { default as AssignmentModal } from './AssignmentModal';
