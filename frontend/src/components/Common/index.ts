d2c78a3e00fc033150816830e281aad7b8578d88
CesarArathAP <cesararath1976@gmail.com>
2026-02-18 16:37:05 -0600

    refactor: eliminar directorios obsoletos Dashboard/, Common/ y utils/

--- frontend/src/components/Common/index.ts
+++
@@ -1,5 +0,0 @@
-// Componentes comunes reutilizables
-// Se agregarán conforme se desarrolle la aplicación
-
-// Placeholder export para que sea un módulo válido
-export const CommonComponents = {};
--- frontend/src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as authService } from './authService';
-export { API_CONFIG, AUTH_CONFIG } from './config';
